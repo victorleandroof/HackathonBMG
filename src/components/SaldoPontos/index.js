b270b438ad7a3f983092f06a345e438ab2805255
Liara <987.liara@gmail.com>
2020-05-31 04:47:13 -0300

    SaldoPontos done

--- src/components/SaldoPontos/index.js
+++ src/components/SaldoPontos/index.js
@@ -3,12 +3,15 @@ import './SaldoPontos.css'
 
 function SaldoPontos() {
   return (
-    <div className="background-saldo">
-      <div className="label">Saldo disponivel</div>
-      <div className="content">R$89,00</div>
-
-      <div className="label">Pontuação</div>
-      <div className="content">240</div>
+    <div className="background-saldo text-saldo-pontos flex-position">
+      <div className="saldo">
+        <div className="label"> Saldo disponivel</div>
+        <div className="content"> R$89,00</div>
+      </div>
+      <div className="pontos">
+        <div className="label">Pontuação</div>
+        <div className="content">240</div>
+      </div>
     </div>
   )
 }
