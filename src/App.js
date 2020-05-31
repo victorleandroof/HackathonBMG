7281585d5543f2de975f504576604ec03edd26be
victorleandroof <samuellv010@hotmail.com>
2020-05-31 00:45:11 -0300

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+      <h1>
+          HELLO WORD
+      </h1>
+  );
+
+}
+
+export default App;
