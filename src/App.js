a189e162281bd2541e1b8615846187f1df43c862
victorleandroof <samuellv010@hotmail.com>
2020-05-31 02:06:00 -0300

    added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
-    <h1>HELLO WORLD</h1>
+    <Header titulo='Extrato' />
   );
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Header.css'
+
+function Header({titulo}) {
+  return (
+  <h1>{titulo}</h1>
+  );
+}
+
+export default Header;
