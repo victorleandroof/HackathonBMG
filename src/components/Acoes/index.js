085821ebd653ba291a2e2d05022eeef9aa07d347
liara987 <987.liara@gmail.com>
2020-05-31 12:20:19 -0300

    Adding acoes

---
+++ src/components/Acoes/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Acoes.css';
+
+function Acoes() {
+  return (
+    <div>                    
+        Acoes
+    </div>
+
+
+
+
+
+  )
+}
+
+export default Acoes;
---
+++ src/services/produtos.js
@@ -0,0 +1,105 @@
+export function getInfo() {
+    return [{
+            titulo: "Americanas",
+            valores: [{
+                    nome: "2015",
+                    valor: 16.17
+                },
+
+                {
+                    nome: "2016",
+                    valor: 16.17
+                },
+
+                {
+                    nome: "2017",
+                    valor: 16.17
+                },
+
+                {
+                    nome: "2018",
+                    valor: 16.17
+                },
+
+                {
+                    nome: "2019",
+                    valor: 16.17
+                },
+
+                {
+                    nome: "2020",
+                    valor: 16.17
+                },
+
+            ]
+        },
+        {
+            titulo: "Magazine Luiza",
+            valores: [{
+                    nome: "2015",
+                    valor: 16.17
+                },
+
+                {
+                    nome: "2016",
+                    valor: 16.17
+                },
+
+                {
+                    nome: "2017",
+                    valor: 16.17
+                },
+
+                {
+                    nome: "2018",
+                    valor: 16.17
+                },
+
+                {
+                    nome: "2019",
+                    valor: 16.17
+                },
+
+                {
+                    nome: "2020",
+                    valor: 16.17
+                },
+
+            ]
+        },
+        {
+            titulo: "Netflix",
+            valores: [{
+                    nome: "2015",
+                    valor: 16.17
+                },
+
+                {
+                    nome: "2016",
+                    valor: 16.17
+                },
+
+                {
+                    nome: "2017",
+                    valor: 16.17
+                },
+
+                {
+                    nome: "2018",
+                    valor: 16.17
+                },
+
+                {
+                    nome: "2019",
+                    valor: 16.17
+                },
+
+                {
+                    nome: "2020",
+                    valor: 16.17
+                },
+
+            ]
+        }
+    ];
+}
