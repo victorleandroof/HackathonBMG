0c2b7ca6b2884f4f0b63affd3eba594869e32b90
liara987 <987.liara@gmail.com>
2020-05-31 14:55:05 -0300

    creating modal page

---
+++ src/pages/Modal/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Modal.css'
+import Quiz from '../../components/Quiz';
+
+function Modal() {
+  return (
+    <Quiz/>
+  )
+}
+
+export default Modal;
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route } from 'react-router-dom'
 
 import Extrato from './pages/ExtratoPage'
 import Acoes from './pages/Acoes'
+import Modal from './pages/Modal'
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Route path="/" exact component={Extrato} />
             <Route path="/acoes" exact component={Acoes} />
+            <Route path="/modal" exact component={Modal} />
         </BrowserRouter>
     )
 }
